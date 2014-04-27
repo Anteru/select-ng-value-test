e75aefa797c2760583c0ff66c0a17ed9ae41b086
Matthäus G. Chajdas <dev@anteru.net>
2014-04-27 08:13:24 +0200

    Initial commit.

---
+++ web/selecttest.dart
@@ -0,0 +1,42 @@
+import 'package:angular/angular.dart';
+import 'package:angular/application_factory.dart';
+
+class Item
+{
+  String name, value;
+  
+  Item (this.name, this.value);
+}
+
+@Controller(
+    selector: '[controller]',
+    publishAs: 'ctrl')
+class Test
+{
+  List<Item> items = [new Item ('Alice', 'one'), new Item ('Bob', 'two')];
+  Item item;
+  
+  Scope scope;
+  
+  Test(Scope this.scope)
+  {
+    scope.watch('item', (newValue, _) {
+      if (newValue != null) {
+        print (newValue.value);
+      }
+    },
+    context: this);
+  }
+}
+
+class TestModule extends Module
+{
+  TestModule ()
+  {
+    type (Test);
+  }
+}
+
+void main() {
+  applicationFactory().addModule(new TestModule()).run();
+}
